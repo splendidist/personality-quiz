74121a710b8f0592e941335d05f85b29ad0bd37e
liam-allman <84319148+liam-allman@users.noreply.github.com>
2024-04-21 18:47:00 +0100

    Remove the dot from image url

--- client/app.js
+++ client/app.js
@@ -56,7 +56,7 @@ let questions = [
       {
         text: "Having a picnic under the stars",
         image:
-          ".https://images.unsplash.com/photo-1596241913242-b20788b3dfeb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "https://images.unsplash.com/photo-1596241913242-b20788b3dfeb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         alt: "picnic",
         tag: "lover",
       },
