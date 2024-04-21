6bb46b350355c9815934339ebfcc0de77350ce97
liam-allman <84319148+liam-allman@users.noreply.github.com>
2024-04-21 18:43:20 +0100

    Updating JS to increment the value for each era

--- client/app.js
+++ client/app.js
@@ -9,22 +9,22 @@ let questions = [
         text: "Horseback riding through open fields",
         image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         alt: 'Horse Riding',
-        tag:"lover"
+        tag:"debut"
       },
       {
         text: "Sitting around a bonfire with friends",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"fearless"
       },
       {
         text: "Going on a hayride through the countryside",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"speak now"
       },
       {
         text: "Visiting a local county fair and enjoying the festivities",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"red"
       }
     ]
   },
@@ -33,23 +33,23 @@ let questions = [
     answers: [
       {
         text: "Writing love letters",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"1989"
       },
       {
         text: "Going on a spontaneous road trip",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"reputation"
       },
       {
         text: "Having a picnic under the stars",
-        image:"./img/something.png",
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         tag:"lover"
       },
       {
         text: "Sending flowers with a heartfelt note",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"folklore"
       }
     ]
   },
@@ -58,23 +58,23 @@ let questions = [
     answers: [
       {
         text: "Professing your love for someone",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"evermore"
       },
       {
         text: "Stopping a mismatched marriage",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"midnights"
       },
       {
         text: "Rekindling an old flame",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"debut"
       },
       {
         text: "Objecting for a friend's happiness",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"fearless"
     }
     ]
   },
@@ -83,23 +83,23 @@ let questions = [
     answers: [
       {
         text: "Fiery red for passion and intensity",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"speak now"
       },
       {
         text: "Cool blue for tranquility and introspection",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"red"
       },
       {
         text: "Vibrant yellow for joy and optimism",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"1989"
       },
       {
         text: "Mysterious black for depth and mystery",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"reputation"
       }
     ]
   },
@@ -108,23 +108,23 @@ let questions = [
     answers: [
       {
         text: "Trendy nightclub in the city",
-        image:"./img/something.png",
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         tag:"lover"
       },
       {
         text: "Cozy coffee shop with live music",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"folklore"
       },
       {
         text: "Retro-themed roller skating rink",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"evermore"
       },
       {
         text: "Rooftop bar overlooking the skyline",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"midnights"
       }
     ]
   },
@@ -133,23 +133,23 @@ let questions = [
     answers: [
       {
         text: "Confronting the source directly",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"debut"
       },
       {
         text: "Ignoring it and letting it die down",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"fearless"
       },
       {
         text: "Addressing it through art or music",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"speak now"
       },
       {
         text: "Embracing it and turning it into a joke",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"red"
       }
     ]
   },
@@ -158,23 +158,23 @@ let questions = [
     answers: [
       {
         text: "Exchanging promise rings with your partner",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"1989"
       },
       {
         text: "Planting a tree together as a symbol of growth and commitment",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"reputation"
       },
       {
         text: "Creating a scrapbook of shared memories and adventures",
-        image:"./img/something.png",
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         tag:"lover"
       },
       {
         text: "Writing love notes and hiding them in unexpected places for your partner to find",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"folklore"
       }
     ]
   },
@@ -183,23 +183,23 @@ let questions = [
     answers: [
       {
         text: "Romance",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"evermore"
       },
       {
         text: "Mystery",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"midnights"
       },
       {
         text: "Fantasy",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"debut"
       },
       {
         text: "Historical fiction",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"fearless"
       }
     ]
   },
@@ -208,23 +208,23 @@ let questions = [
     answers: [
       {
         text: "A journal and pen to capture your thoughts and inspirations",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"speak now"
       },
       {
         text: "A stack of your favourite books to get lost in",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"red"
       },
       {
         text: "A guitar or musical instrument to create melodies surrounded by nature",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"1989"
       },
       {
         text: "Ingredients for baking or cooking cozy meals by the fireplace",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"reputation"
       }
     ]
   },
@@ -233,23 +233,23 @@ let questions = [
     answers: [
       {
         text: "Stargazing alone on a quiet rooftop",
-        image:"./img/something.png",
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         tag:"lover"
       },
       {
         text: "Taking a solitary walk under the moonlight",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"folklore"
       },
       {
         text: "Writing poetry or journaling by candlelight",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"evermore"
       },
       {
         text: "Listening to music with headphones on, lost in your thoughts",
-        image:"./img/something.png",
-        tag:"lover"
+        image:"https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tag:"midnights"
       }
     ]
   }
@@ -260,70 +260,70 @@ let result = [
   {
     era: "debut",
     description: "dkljfalfjna",
-    image: "./img/something.png",
+    image: "https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Image Alt",
     value: 0
   },
   {
     era: "fearless",
     description: "dkljfalfjna",
-    image: "./img/something.png",
+    image: "https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Image Alt",
     value: 0
   },
   {
     era: "speak now",
     description: "dkljfalfjna",
-    image: "./img/something.png",
+    image: "https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Image Alt",
     value: 0
   },
   {
     era: "red",
     description: "dkljfalfjna",
-    image: "./img/something.png",
+    image: "https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Image Alt",
     value: 0
   },
   {
     era: "1989",
     description: "dkljfalfjna",
-    image: "./img/something.png",
+    image: "https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Image Alt",
     value: 0
   },
   {
     era: "reputation",
     description: "dkljfalfjna",
-    image: "./img/something.png",
+    image: "https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Image Alt",
     value: 0
   },
   {
     era: "lover",
     description: "dkljfalfjna",
-    image: "./img/something.png",
+    image: "https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Image Alt",
     value: 0
   },
   {
     era: "folklore",
     description: "dkljfalfjna",
-    image: "./img/something.png",
+    image: "https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Image Alt",
     value: 0
   },
   {
     era: "evermore",
     description: "dkljfalfjna",
-    image: "./img/something.png",
+    image: "https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Image Alt",
     value: 0
   },
   {
     era: "midnights",
     description: "dkljfalfjna",
-    image: "./img/something.png",
+    image: "https://images.unsplash.com/photo-1532876075969-8c016daf9242?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Image Alt",
     value: 0
   }
@@ -332,6 +332,8 @@ let result = [
 // Element Selection
 const questionTag = document.querySelector('h2');
 const startQuiz = document.querySelector('button');
+const introText = document.getElementById('intro-text');
+const optionButtons = document.getElementsByClassName('image-button');
 
 // Button Wrapper
 const answersWrapper = document.getElementById('answers');
@@ -359,11 +361,15 @@ const answerFourQuestion = document.createElement('p');
 
 // Sets the loop index
 let i = 0;
+let chosenAnswer = null;
 
 // Element content creation / change function
 function displayQuestion(event) {
   // Append new buttons to the wrapper
   if (i == 0) {
+    introText.style.display = "none";
+    startQuiz.textContent = 'Next';
+
     answersWrapper.appendChild(answerOne);
     answerOne.setAttribute("class", "image-button");
     answerOne.appendChild(answerOneButton);
@@ -391,6 +397,20 @@ function displayQuestion(event) {
 
   // Updates the page content
   if (i <= 9) {
+    // For loop for the button on the page
+    for (let i = 0; i < optionButtons.length; i++) {
+      optionButtons[i].addEventListener('click', function(){
+        chosenAnswer = answers[i].tag;
+      });
+    }
+
+    // forEach over the result array and increment the value property
+    result.forEach(function(resultsObject) {
+      if (resultsObject.era === chosenAnswer) {
+        resultsObject.value++;
+      }
+    });
+
     // Updates header H2 to be current question
     questionTag.textContent = questions[i].question;
   
@@ -407,8 +427,32 @@ function displayQuestion(event) {
     answerOneImg.src = answers[0].image;
     answerOneImg.alt = answers[0].alt;
 
+    answerTwoImg.src = answers[1].image;
+    answerTwoImg.alt = answers[1].alt;
+
+    answerThreeImg.src = answers[2].image;
+    answerThreeImg.alt = answers[2].alt;
+
+    answerFourImg.src = answers[3].image;
+    answerFourImg.alt = answers[3].alt;
+
     i++;
   } else {
+    // forEach over the result array and increment the value property
+    result.forEach(function(resultsObject) {
+      if (resultsObject.era === chosenAnswer) {
+        resultsObject.value++;
+      }
+    });
+
+    result.forEach(function(resultsObject) {
+      if (resultsObject.era === chosenAnswer) {
+        resultsObject.value++;
+      }
+      
+      // console.log(resultsObject.era + ' : ' + resultsObject.value);
+    });
+
     console.log('add result here');
   }
 }
